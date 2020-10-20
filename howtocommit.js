79ef30c301d80411f449b9cbde8c6d04b846e216
deepanshugarg195@gmail.com <deepanshugarg195@gmail.com>
2020-10-20 23:28:38 +0530

    fix(and): the fix change
    Added new Title
    forward popstate event if available
    
    BREAKING CHANGE: this would be a breaking change

---
+++ howtocommit.js
@@ -0,0 +1,9 @@
+// fix(subject):"commit messgae"      1.0.0 -> 1.0.1
+//feat(subject): "any message"     1.0.0 -> 1.1.0
+/* feat(subject):"message  
+1 line gap
+body
+2 line gap 
+BREAKING CHANGES: "anything you want to print"
+            1.0.0->2.0.0
+**/
