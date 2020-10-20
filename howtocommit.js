032e97bc0d79bdd0b88653f8084a979155b9af27
deepanshugarg195@gmail.com <deepanshugarg195@gmail.com>
2020-10-20 23:29:41 +0530

    fix(and): the fix change

--- howtocommit.js
+++ howtocommit.js
@@ -7,3 +7,8 @@ body
 BREAKING CHANGES: "anything you want to print"
             1.0.0->2.0.0
 **/
+// https://github.com/angular/angular.js/blob/master/DEVELOPERS.md#-git-commit-guidelines
+// https://docs.google.com/document/d/1QrDFcIiPjSLDn3EL15IJygNPiHORgU1_OOAqWjiDU5Y/edit#
+// https://github.com/semantic-release/commit-analyzer#release-rules
+// https://github.com/semantic-release/semantic-release/issues/370
+// https://github.com/semantic-release/semantic-release
